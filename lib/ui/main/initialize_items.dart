1fa65743350b97fb5ca0e44a4bfb25c9f2144a31
xiang.zhou <xiang.zhou2@amh-group.com>
2022-07-14 10:09:20 +0800

    feat |> update flutter 3.0

--- lib/ui/main/initialize_items.dart
+++ lib/ui/main/initialize_items.dart
@@ -2,18 +2,9 @@ import 'package:favorcate/ui/pages/favor/favor_screen.dart';
 import 'package:favorcate/ui/pages/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-final List<Widget> pages = [
-  HomeScreen(),
-  FavorScreen()
-];
+final pages = [HomeScreen(), FavorScreen()];
 
-final List<BottomNavigationBarItem> items = [
-  BottomNavigationBarItem(
-    title: Text("首页"),
-    icon: Icon(Icons.home)
-  ),
-  BottomNavigationBarItem(
-      title: Text("收藏"),
-      icon: Icon(Icons.star)
-  ),
+final items = [
+  BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
+  BottomNavigationBarItem(icon: Icon(Icons.star), label: "收藏"),
 ];
